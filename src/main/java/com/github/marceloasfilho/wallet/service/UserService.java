d68727800aca6deeea05fc943912c0d6a0d2c605
marceloasfilho <marceloandradesilvafilho@gmail.com>
2022-09-27 18:27:51 -0300

    Reformatando código

--- src/main/java/com/github/marceloasfilho/wallet/service/UserService.java
+++ src/main/java/com/github/marceloasfilho/wallet/service/UserService.java
@@ -5,6 +5,7 @@ import com.github.marceloasfilho.wallet.entity.User;
 import java.util.Optional;
 
 public interface UserService {
-    User save (User user);
+    User save(User user);
+
     Optional<User> findByEmail(String email);
 }
