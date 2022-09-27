711439566981de66d5910301713da383a6b82f7c
marceloasfilho <marceloandradesilvafilho@gmail.com>
2022-09-27 00:07:56 -0300

    first commit

---
+++ src/test/java/com/github/marceloasfilho/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marceloasfilho.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
